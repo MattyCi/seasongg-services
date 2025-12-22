96abbbddae388fd635297462ff8c7bb1076cf202
matt <mjciaravino@gmail.com>
2025-12-21 19:57:23 -0500

    WIP: work on daos

--- src/main/java/com/sgg/rounds/model/RoundResultDao.java
+++ src/main/java/com/sgg/rounds/model/RoundResultDao.java
@@ -1,4 +1,23 @@
 package com.sgg.rounds.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "round_results")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RoundResultDao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "round_result_id")
+    private Long roundResultsId;
+
+
+    private Double points;
+
 }
